1241333b6dc47c2f974680afbce72767118d7057
AnriNakamura <an12inakamu12a@gmail.com>
2023-06-07 23:02:01 +0900

    update

--- rust/week1/anagram1/src/main.rs
+++ rust/week1/anagram1/src/main.rs
@@ -1,8 +1,22 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+// 文字列を ASCII コードのベクタに変換する
+fn string_to_bytes(word: &str) -> Vec<u8> {
+    let mut bytes: Vec<u8> = Vec::new();
+    for c in word.bytes() {
+        println!("{:x}", c);
+    }
+    return bytes;
+}
+
+
 // アナグラムになる単語のリストを返す
-fn find_anagram(random_word: &Vec<String>, dictionary: &Vec<Vec<String>>) -> Vec<String> {
+fn find_anagram(word: &str, dictionary: &Vec<Vec<String>>) -> Vec<String> {
     let mut anagrams: Vec<String> = Vec::new();
 
-    let sorted_random_word = random_word.sort();
+    let mut sorted_word = string_to_bytes(word).sort();
 
     // Note: sort ができないのでデータ構造を考え直す必要がある？
     let mut new_dictionary: Vec<(&Vec<String>, &Vec<String>)> = Vec::new();
@@ -15,7 +29,7 @@ fn find_anagram(random_word: &Vec<String>, dictionary: &Vec<Vec<String>>) -> Vec
 
 
 // 二分探索して、アナグラムになりうる単語を探す
-fn binary_search(word: &Vec<String>, dict: &Vec<(&Vec<String>, &Vec<String>)>) -> Vec<Vec<&String>> {
+fn binary_search(word: str, dict: &Vec<(&Vec<String>, &Vec<String>)>) -> Vec<Vec<&String>> {
     let left: usize = 0;
     let right: usize = dict.len() - 1;
 
@@ -54,8 +68,18 @@ fn binary_search(word: &Vec<String>, dict: &Vec<(&Vec<String>, &Vec<String>)>) -
 
 
 // [未実装] ファイルの読み込み
-fn read_file(file_name: &str) {
-    
+fn read_file(file_name: &String) {
+    let mut a_file = File::open(file_name)
+    // ファイルが見つからない
+    .expect("file not found");
+
+    let mut dictionary = String::new();
+    a_file.read_to_string(&mut dictionary)
+        // ファイルの読み込み中に問題が発生
+        .expect("something went wrong reading the file");
+
+    // テキストは \n{}
+    println!("With text:\n{}", dictionary);   
 }
 
 
@@ -65,9 +89,31 @@ fn write_anagram(list_of_anagram: &Vec<Vec<String>>) {
 }
 
 
+struct Config {
+    file_name: String, 
+    word: String,
+}
+
+
+fn parse_config(args: &[String]) -> Config {
+    let file_name = args[1].clone();
+    let word = args[2].clone();
+
+    Config { file_name, word }
+}
+
+
 // [未実装]
 fn main() {
     // 引数の受け取り
+    // anagram1.py dictionary_file a_word
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let config = parse_config(&args);
+    println!("file: {}, word: {}", config.file_name, config.word);
+
+    read_file(&config.file_name);
 
     // dictionary = read_file(file_name);
     // answer = find_anagram(random_word, dictionary);
