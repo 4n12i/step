915ff5835911bb2e2cdf30b789135f159f1ab320
AnriNakamura <an12inakamu12a@gmail.com>
2023-06-22 23:35:24 +0900

    update

--- python/week5/solver_original.py
+++ python/week5/solver_original.py
@@ -37,15 +37,21 @@ def find_cross_line(tour, cities):
         return True
 
 
-    for i in range(len(tour)-1,0,-1):
-        for k in range(len(tour)-1): # 
-            if tour[k] == tour[i] or tour[k] == tour[i-1] or tour[k + 1] == tour[i] or tour[k + 1] == tour[i-1]:
-            # if i - 1 == k and k <= i:
+    n = len(tour)
+    for i in range(n-1,0,-1):   # TODO: 交差した線がなくなるまで繰り返す
+        for k in range(i):  # TODO: k = n-1 のときは k + 1 じゃなくて 0 にする
+            if k == i - 1 or k == i - 2:
                 continue
-            if is_cross(tour[i], tour[i - 1], tour[k], tour[k + 1]):
-                # print(f"found cross line! a:{tour[i - 1]} b:{tour[i]} c:{tour[k]} d:{tour[k + 1]}, {i,i-1,k,k+1}")
-                tour[i - 1], tour[k + 1] = tour[k + 1], tour[i - 1] # ab, cd -> ac, bd
-                break
+
+            if is_cross(tour[i-1], tour[i], tour[k], tour[k + 1]):
+                # print(f"cross a:{tour[i - 1]} b:{tour[i]} c:{tour[k]} d:{tour[k + 1]}, {i-1,i,k,k+1}")
+
+                # 入れ替えた点と点の間の経路を反転する
+                if i > k:
+                    tour = tour[:k+1] + tour[k+1:i+1][::-1] + tour[i+1:]
+                else:
+                    tour = tour[:i] + tour[i:k+1][::-1] + tour[k+1:]
+                break   
     
     return tour
 
@@ -65,14 +71,14 @@ def solve(cities):
 
     # 経路を求める
     while unvisited_cities:
-        # 一番距離が短い
+        # 一番距離が短い点と点のペアを探し、経路 tour に挿入する
         next_city = min(unvisited_cities,
                         key=lambda city: dist[current_city][city])
         unvisited_cities.remove(next_city)
         tour.append(next_city)
         current_city = next_city
 
-    tour = find_cross_line(tour, cities)
+    tour = find_cross_line(tour, cities) # 交差している2線を交換する（2-opt）
 
     return tour
 
